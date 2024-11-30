e78f37161d55a93aa20b3948c67581acce2eac3f
happyADD <052310228@nuaa.edu.cn>
2024-11-30 12:39:30 +0800

    确定目标

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # manimshow
+
+This project uses manim library to create animations.
+
+### environment setup
+python 3.7+
+manim library
+vscode or any other IDE
+
+
+## What I want to do is make Schrodinger's equation animation.
+Just to do : 
+1. deduce the Schrodinger equation from the Hamiltonian and the wave function equation of one-dimensional electron in infinite deep potential well.
+2. learn how to use manim library to create animations.
+3. create the animation.
--- Schrodinger.py
+++ Schrodinger.py
@@ -4,6 +4,21 @@ from manim import *
 class Scene(Scene):
 
     def construct(self):
+
+        """ 序幕，介绍 """
+        preface_text = Text("我们将讨论一维无限深势井中的单电子的波函数\n并解释量子纠缠的基本原理。").to_edge(UP).set_color(BLUE)
+        self.play(Write(preface_text))
+        self.wait(2)
+        self.play(FadeOut(preface_text))
+
+        text = Text("这是一维无限深势井的函数：").to_edge(UP).set_color(BLUE)
+        equation = Tex(r"|\Psi(x,t)|^2 = e^{-|\frac{x}{2\pi}\left(1-\cos(2\pi f_0 t)\right)|^2}").scale(1.5).next_to(text,DOWN).set_color(YELLOW)
+
+        self.play(Create(text), Create(equation))
+        self.wait(1)
+
+        """ 第一幕，从薛定谔方程到氢原子定式解"""
+
         # 创建文本和方程
         text = Text("Schrodinger Equation:").to_edge(UP).set_color(BLUE)
         equation = Tex(r"$i\hbar\frac{\partial}{\partial t}\Psi(\mathbf{r}, t) = \hat{H}\Psi(\mathbf{r}, t)$").scale(1.5).next_to(text,DOWN).set_color(YELLOW)
@@ -14,7 +29,6 @@ class Scene(Scene):
         text_that_is = Text("即:").set_color(BLUE).to_edge(LEFT)
         H2_equation = Tex(r"$\Psi_{H_2}(r) = \frac{1}{\sqrt{\pi a_0^3}} e^{-r/a_0} $").set_color(YELLOW).scale(2).center()
 
-        """ 第一幕，从薛定谔方程到氢原子定式解"""
         self.play(Create(text), Create(equation))
         self.wait(1)
 
@@ -33,14 +47,14 @@ class Scene(Scene):
         equation2 = Tex(r"$\Psi_{H_2}(x) = \frac{1}{\sqrt{\pi a_0^3}} e^{-x/a_0} $").set_color(YELLOW).scale(2).next_to(text,DOWN)
 
         text_eula = Text("由欧拉公式可化为：").to_corner(UL).set_color(BLUE)
-        equation3 = Tex(r"$\Psi_{H_2}(x) = \frac{1}{\sqrt{\pi a_0^3}}(\cos(-\frac{x}{a_0})+\i\sin(-\frac{x}{a_0})) $").set_color(YELLOW).scale(1.5).to_edge(LEFT)
+        equation3 = Tex(r"$\Psi_{H_2}(x) = \frac{1}{\sqrt{\pi a_0^3}}(\cos(-\frac{x}{a_0})+i\sin(-\frac{x}{a_0})) $").set_color(YELLOW).scale(1.5).to_edge(LEFT)
 
         self.play(Create(text))
         self.wait(1)
         self.play(Transform(equation,equation2))
         self.wait(1)
         self.play(Transform(text,text_eula), Transform(equation,equation3))
-        self.wait(1)
+        self.wait(5)
         
 
 
